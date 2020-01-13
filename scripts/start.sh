f27cf95f943c85b5ffa5210d1ec12690854c4f1b
kessibi <guillaume@kessibi.fr>
2020-01-13 11:53:22 +0100

    start: chown on /etc/airtime to be able to create conf file

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 mkdir -p /srv/airtime/stor
-chown -R www-data:www-data /srv/airtime/
+chown -R www-data:www-data /srv/airtime
+chown -R www-data:www-data /etc/airtime
 
 pg_ctlcluster 10 main restart
 
